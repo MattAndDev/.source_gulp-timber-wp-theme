7b676171d5bc6d4c729217070f3662a3c13e8e50
Matteo <me@mattinthe.net>
2016-01-24 19:09:28 +0100

    Removed import css

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,17 +7,15 @@ var sourcemaps   = require('gulp-sourcemaps');
 var handleErrors = require('../util/handleErrors');
 var config       = require('../config').sass;
 var autoprefixer = require('gulp-autoprefixer');
-var importCss    = require('gulp-import-css');
 
 gulp.task('sass', function () {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
     .on('error', handleErrors)
-    .pipe(sourcemaps.write())
     .pipe(autoprefixer({ browsers: config.prefix }))
-    .pipe(importCss())
     .on('error', handleErrors)
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
