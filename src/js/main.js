e60039ab5230b56f516f38e00873955afc7be9bd
Matt <mma@arillo.net>
2016-01-26 15:18:46 +0100

    Rm stupid plugin example, added feature js a shimemd plugin

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,27 @@
 import $ from 'jquery';
 
-// Shimmed plugin example
-import plugin from 'plugin';
+// Shimmed feature js , aiting for npm module
+import featurejs from 'featurejs';
 
 // Enable inline svgs in IE
 // import svg4everybody from 'svg4everybody';
 // svg4everybody();
 
 $(() => {
-  // Shimmed plugin example
-  console.log('test');
-  plugin();
+
+  // =======================================================
+  // Frontend helpers
+  // Init featurejs and set default touch/no-touc flag
+  // =======================================================
+
+  featurejs();
+  if (feature.touch) {
+    $('html').addClass('touch');
+  } else {
+    $('html').addClass('no-touch');
+  }
+  // =======================================================
+
+  // Code some awesome things
+
 });
